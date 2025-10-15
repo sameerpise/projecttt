b8302ae49f3820be92040820e8aba1ac4ac7379f
Sameer Laxman PIse <79313336+sameerpise@users.noreply.github.com>
2025-10-15 18:46:09 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -15,7 +15,18 @@ const allowedOrigins = [
   "http://localhost:5173",               // local dev
   "https://projecttt-1611.onrender.com"  // your deployed frontend
 ];
-app.use(cors({ origin:allowedOrigins , credentials: true }));
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin (like Postman)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
