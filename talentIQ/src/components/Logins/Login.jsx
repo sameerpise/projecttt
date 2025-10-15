282493069f2a9f71a5156fee269f6c73d4e0518f
Sameer Laxman PIse <79313336+sameerpise@users.noreply.github.com>
2025-10-15 17:47:42 +0530

    Update Login.jsx

--- talentIQ/src/components/Logins/Login.jsx
+++ talentIQ/src/components/Logins/Login.jsx
@@ -47,7 +47,7 @@ export default function Login() {
       }
     } else {
       try {
-        const res = await fetch("http://localhost:5000/api/students/login", {
+        const res = await fetch("https://projecttt-15.onrender.com/api/students/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(form),
