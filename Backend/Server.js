2711f37e7db9a5c46e36f533dbaedc948ccc5ef3
Sameer Laxman PIse <79313336+sameerpise@users.noreply.github.com>
2025-10-15 18:33:07 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173","https://projecttt-1678.onrender.com", credentials: true }));
 app.use(express.json());
 
 // Routes
